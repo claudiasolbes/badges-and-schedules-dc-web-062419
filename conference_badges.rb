029c6a44f31301e17eb00d612f4682ab5806f1df
Claudia Solbes <cssolbes@hotmail.com>
2019-06-07 17:10:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,5 +19,15 @@ def assign_rooms (attendees)
 end
 
 def printer(attendees)
-
+badges = []
+  attendees.each do |names|
+   badges << "Hello, my name is #{names}."
+  end
+  return badges
+end
+name_room = []
+  attendees.each_with_index do |attendees, room_assignments|
+    name_room << "Hello, #{attendees}! You'll be assigned to room #{room_assignments + 1}!"
+  end
+  return name_room
 end

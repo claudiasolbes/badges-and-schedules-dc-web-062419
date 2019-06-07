fc93a51270791dd6b3436b7eaf5632bb9ab28f8e
Claudia Solbes <cssolbes@hotmail.com>
2019-06-07 15:36:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,6 +6,7 @@ def batch_badge_creator(attendees)
   badges = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   
   badges.each do |names|
+    all_badges = []
   return "Hello, my name is #{names}."
   end
 end
